a47acaf16332d98799dacbc9c3e9a85a4d137cd4
Karolina Maciejewska <kmaciejewska14@gmail.com>
2022-04-28 19:41:40 +0200

    format string

--- fakeJSON/index.ts
+++ fakeJSON/index.ts
@@ -3,5 +3,13 @@ import axios from "axios";
 const url = "https://my-json-server.typicode.com/typicode/demo/comments/1";
 
 axios.get(url).then((response) => {
-  console.log(response.data);
+  const todo = response.data;
+
+  const id = todo.id;
+  const body = todo.body;
+
+  console.log(`
+    The todo with ID: ${id}
+    Has a body of: ${body}
+  `);
 });
