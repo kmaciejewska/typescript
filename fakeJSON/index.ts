4859fc613e84e225636441d39fa46af275a71030
Karolina Maciejewska <kmaciejewska14@gmail.com>
2022-04-28 19:52:24 +0200

    add some ts

--- fakeJSON/index.ts
+++ fakeJSON/index.ts
@@ -2,14 +2,23 @@ import axios from "axios";
 
 const url = "https://my-json-server.typicode.com/typicode/demo/comments/1";
 
+interface Todo {
+  id: number;
+  body: string;
+}
+
 axios.get(url).then((response) => {
-  const todo = response.data;
+  const todo = response.data as Todo;
 
   const id = todo.id;
   const body = todo.body;
 
+  logTodo(id, body);
+});
+
+const logTodo = (id: number, body: string) => {
   console.log(`
     The todo with ID: ${id}
     Has a body of: ${body}
   `);
-});
+};
